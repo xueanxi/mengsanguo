50521bbb79b3693d199ee38f3a9293b77c75cd9d
anxi.xue <anxi.xue@tcl.com>
2017-11-30 10:37:42 +0800

    nong yao shua jin bu zhou shou.

--- Mengsanguo/app/src/main/java/mengsanguo/anxi/com/mengsanguo/utils/LogUtils.java
+++
@@ -1,7 +0,0 @@
-package mengsanguo.anxi.com.mengsanguo.utils;
-
-/**
- * Created by user on 5/24/17.
- */
-public class LogUtils {
-}
--- Mengsanguo/app/src/main/java/mengsanguo/anxi/com/mengsanguo/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package mengsanguo.anxi.com.mengsanguo.utils;
-
-/**
- * Created by user on 5/23/17.
- */
-public class Utils {
-}
